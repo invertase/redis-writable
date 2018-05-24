8657a4d5de351ded1735db62c471fa3f21b1ce89
Salakar <mike.diarmid@gmail.com>
2018-05-24 23:27:13 +0100

    add byteLength benchmarks

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,16 +1,27 @@
-'use strict';
-
-
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite();
-var toWritable = require('./../');
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite();
+const toWritable = require('./../');
 
 console.log('\r\n');
 require('./print');
 console.log('\r\n');
 
+const largeStr = new Array(4096 + 1).join('-');
 
 suite
+  .add('byteLength small', function () {
+    toWritable.byteLength('PING');
+  })
+  .add('byteLength small - native', function () {
+    Buffer.byteLength('PING', 'utf8')
+  })
+  .add('byteLength large', function () {
+    toWritable.byteLength(largeStr);
+  })
+  .add('byteLength large - native', function () {
+    Buffer.byteLength(largeStr, 'utf8')
+  })
+
   .add('0 ARGS', function () {
     return toWritable('PING');
   })
@@ -21,10 +32,10 @@ suite
     return toWritable('INCR', ['moo', 1]);
   })
   .add('3 args', function () {
-    return toWritable('PING', ['moo', 'bar', 'far']);
+    return toWritable('PING', ['moo', 0, 1]);
   })
   .add('4+ args', function () {
-    return toWritable('PING', ['moo', 'bar', 'far', 'car', 'rar']);
+    return toWritable('PING', [0, 1, undefined, () => {}, {}, 'foo']);
   })
   .on('cycle', function (e) {
     console.log('' + e.target);
